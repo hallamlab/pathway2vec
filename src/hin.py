03765d7615e581639c3fc3ba024c6520ede5d289
atante <arbasher@alumni.ubc.ca>
2020-03-15 04:40:34 -0700

    update

--- src/hin.py
+++ src/hin.py
@@ -449,7 +449,6 @@ class MetaPathGraph(object):
         logger.info('\t>> Calculate initial transition probabilities...')
         N = (hin.number_of_nodes(), hin.number_of_nodes())
         trans_prob = lil_matrix((N[0], N[0]))
-        trans_prob.data[:] = EPSILON
         for curr_node, curr_node_data in hin.nodes(data=True):
             neigh_curr_node = np.array(
                 [hin.nodes[edge[1]]['mapped_idx'] for edge in hin.edges(curr_node)])
