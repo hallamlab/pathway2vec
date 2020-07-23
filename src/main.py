a71db35e3bdd227bcc27bc27f6c2a1d6b847ac4b
atante <arbasher@alumni.ubc.ca>
2020-07-23 00:30:59 -0700

    update

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 __author__ = "Abdurrahman M. A. Basher"
 __date__ = '01/03/2019'
 __copyright__ = "Copyright 2019, The Hallam Lab"
-__license__ = "GPL"
+__license__ = "GPL v3"
 __version__ = "1.0"
 __maintainer__ = "Abdurrahman M. A. Basher"
 __email__ = "ar.basher@alumni.ubc.ca"
