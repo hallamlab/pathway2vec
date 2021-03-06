577eabec5b91a31e1e7d300c02c822f8f020f773
arbasher <arbasher@student.ubc.ca>
2021-03-05 19:45:01 -0800

    update

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-__author__ = "Abdur Rahman M. A. Basher"
-__date__ = '03/08/2020'
-__copyright__ = "Copyright 2020, The Hallam Lab"
-__license__ = "GPL v3"
+__author__ = "Abdurrahman Abul-Basher"
+__date__ = '15/01/2019'
+__copyright__ = "Copyright 2019, The Hallam Lab"
+__license__ = "GPL"
 __version__ = "1.0"
-__maintainer__ = "Abdur Rahman M. A. Basher"
-__email__ = "arbasher@alumni.ubc.ca"
+__maintainer__ = "Abdurrahman Abul-Basher"
+__email__ = "arbasher@student.ubc.ca"
 __status__ = "Production"
 __description__ = "This file is the main entry to perform learning and prediction on dataset using path2vec model."
 
